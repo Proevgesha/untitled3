c6ea2cabc5e06eb4259e3579bfa6b71b12e58ad8
Snigger69 <malahovskiyevgeniy00@gmail.com>
2018-04-29 15:44:20 +0300

    Initial commit

---
+++ src/JustHello.java
@@ -0,0 +1,20 @@
+public class JustHello {
+    public static void main(String[] args) {
+        int [] a = {1,5,3,2,4,1,9};
+        boolean flag = false;
+        while (flag == false) {
+            flag = true;
+            for (int i = 0; i < a.length - 1; i++) {
+                int b = a[i];
+                if (a[i + 1] < b) {
+                    flag = false;
+                    a[i] = a[i + 1];
+                    a[i + 1] = b;
+                }
+            }
+        }
+        for (int c: a) {
+            System.out.println(c);
+        }
+    }
+}
